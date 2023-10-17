9e02551c2bee628d084691586ffe9d2faa67c1fb
Nastia <nastiakopylova1@mail.ru>
2023-10-17 16:49:00 +0300

    implemented route and controller with login logic

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,6 +1,21 @@
 import crypto from 'crypto';
+import { ethers } from 'ethers';
+import jwt from 'jsonwebtoken';
 
 export const getNonceController = (req, res) => {
   const nonce = crypto.randomBytes(32).toString('hex');
   res.status(201).json({ nonce });
+};
+
+export const loginPostController = (req, res) => {
+  const { signedMessage, message, address } = req.body;
+
+  const recoveredAddress = ethers.utils.verifyMessage(message, signedMessage);
+
+  if (recoveredAddress !== address) {
+    return res.status(401).json({ error: { message: 'Invalid signature', status: 401 } });
+  }
+
+  const token = jwt.sign({ address }, process.env.JWT_SECRET, { expiresIn: '10m' });
+  res.status(200).json(token);
 };
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import {
   getNonceController, 
+  loginPostController,
 } from '../controllers/authControllers.js';
 
 const router = express.Router();
 
 router.get('/nonce', getNonceController);
+
+router.post('/login', loginPostController);
 
 export default router;
