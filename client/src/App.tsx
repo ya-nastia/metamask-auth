78a63720871b7dde6fb4b6573a12763728ec4f25
Nastia <nastiakopylova1@mail.ru>
2023-10-19 13:26:56 +0300

    implemented routing and created styles for pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import LoginPage from './pages/LoginPage/LoginPage';
+import DashboardPage from './pages/DashboardPage/DashboardPage';
+import './App.scss';
 
 function App() {
   return (
-    <div>
-
+    <div className="container">
+      <h1>Metamask Login</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<LoginPage />}/>
+          <Route path='/dashboard' element={<DashboardPage />}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
