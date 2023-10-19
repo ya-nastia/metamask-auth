9b5b15db9f71c47af4acb2041aa30e51960abca9
Nastia <nastiakopylova1@mail.ru>
2023-10-19 13:10:12 +0300

    implemented login and dashboard page layout

---
+++ client/src/pages/DashboardPage/DashboardPage.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const DashboardPage: React.FC = () => {
+
+  const logout = () => {}
+
+  return (
+    <div className='btn-container'>
+      <button onClick={logout}>Logout</button>
+    </div>
+  )
+}
+
+export default DashboardPage;
---
+++ client/src/pages/LoginPage/LoginPage.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const LoginPage: React.FC = () => {
+
+  const login = async () => {}
+
+  return (
+    <div className='btn-container'>
+      <button onClick={login}>Login</button>
+    </div>
+  )
+}
+
+export default LoginPage;
